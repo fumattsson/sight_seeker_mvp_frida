418e0cb94c9d112ded20d7d529eff8ab085e927f
Cherry <cherry@Cherrys-iMac.local>
2023-12-09 22:06:53 +0100

    Test branch ver 1

---
+++ composeApp/src/iosMain/kotlin/org.kmp.testing/IOSRuntime.kt
@@ -0,0 +1,10 @@
+package org.kmp.testing
+
+import kotlin.experimental.ExperimentalNativeApi
+import kotlin.native.Platform
+
+@OptIn(ExperimentalNativeApi::class)
+actual fun determineCurrentRuntime(): CurrentRuntime {
+    val name = Platform.osFamily.name.lowercase()
+    return CurrentRuntime(name, null)
+}
